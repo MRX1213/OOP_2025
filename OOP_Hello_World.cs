f759f571f59e280785427d439442ccdf627d0193
Andrii <andreivarlamov228@gmail.com>
2025-01-14 19:56:06 -0500

    More Updates

--- OOP_Hello_World.cs
+++ OOP_Hello_World.cs
@@ -1,20 +1,25 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace HelloWorld
+
+namespace OOP_NET1
 {
     public class Car
     {
-<<<<<<< HEAD
-        string make;
-        string model;
-        int year;
-        string VIN;
-        string interiorColor;
-        string exteriorColor;
+        public string make;
+        public string model;
+        public int year;
+        public string VIN;
+        public string interiorColor;
+        public string exteriorColor;
         public Car(string make, string model, int year, string VIN, string interiorColor, string exteriorColor)
-=======
-        static void Main()
->>>>>>> a73caa16abc074f2119e5950f9a8ac96c7a8600f
         {
             this.make = make;
             this.model = model;
